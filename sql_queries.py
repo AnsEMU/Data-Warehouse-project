55645f4e3131b467c928e78dbd047d6401433805
Ans Elbousiffi <AnsEMU@users.noreply.github.com>
2021-06-22 14:03:56 +0200

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -4,12 +4,10 @@ import configparser
 # CONFIG
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
-IAM_ROLE = config.get('IAM_ROLE','ARN')
-LOG_DATA = config.get('S3','LOG_DATA')
-LOG_JSONPATH = config.get('S3','LOG_JSONPATH')
-SONG_DATA = config.get('S3','SONG_DATA')
-
-
+IAM_ROLE = config.get('IAM_ROLE', 'ARN')
+LOG_DATA = config.get('S3', 'LOG_DATA')
+LOG_JSONPATH = config.get('S3', 'LOG_JSONPATH')
+SONG_DATA = config.get('S3', 'SONG_DATA')
 
 # DROP TABLES
 
@@ -23,7 +21,7 @@ time_table_drop = "DROP TABLE IF EXISTS dimtime;"
 
 # CREATE TABLES
 
-staging_events_table_create= """ 
+staging_events_table_create = """
 CREATE TABLE IF NOT EXISTS staging_events(
      event_id int identity(0,1),
      artist varchar,
@@ -46,7 +44,7 @@ CREATE TABLE IF NOT EXISTS staging_events(
      userId int);
 """
 
-staging_songs_table_create= (""" 
+staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs(
      num_songs int not null,
      artist_id varchar,
@@ -119,7 +117,7 @@ credentials 'aws_iam_role={}'
 json '{}' region 'us-west-2'
 EMPTYASNULL
 BLANKSASNULL;
-""").format(LOG_DATA,IAM_ROLE,LOG_JSONPATH)
+""").format(LOG_DATA, IAM_ROLE, LOG_JSONPATH)
 
 staging_songs_copy = ("""
 copy staging_songs from '{}'
@@ -127,13 +125,15 @@ credentials 'aws_iam_role={}'
 json 'auto' region 'us-west-2'
 EMPTYASNULL
 BLANKSASNULL;
-""").format(SONG_DATA,IAM_ROLE)
+""").format(SONG_DATA, IAM_ROLE)
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
-INSERT INTO factsongplays (start_time, userId, level, song_id, artist_id, sessionId, location, userAgent)
-SELECT DISTINCT timestamp 'epoch' + e.ts * interval '0.001 seconds' as start_time,
+INSERT INTO factsongplays (start_time, userId, level, song_id, artist_id,
+                           sessionId, location, userAgent)
+SELECT DISTINCT timestamp 'epoch' + e.ts * interval '0.001 seconds'
+       as start_time,
        e.userId,
        e.level,
        s.song_id,
@@ -176,19 +176,21 @@ ON (e.artist = s.artist_name)
 """)
 
 artist_table_insert = ("""
-INSERT INTO dimartists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
+INSERT INTO dimartists (artist_id, artist_name, artist_location,
+                        artist_latitude, artist_longitude)
 select artist_id,
        artist_name,
        artist_location,
        artist_latitude,
-       artist_longitude      
+       artist_longitude
      from staging_songs
      WHERE artist_location IS NOT NULL and artist_latitude IS NOT NULL
 """)
 
 time_table_insert = ("""
 INSERT INTO dimtime (start_time, hour, day, week, month, year, weekday)
-SELECT DISTINCT TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second' as start_time,
+SELECT DISTINCT TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second'
+       as start_time,
 EXTRACT (HOUR FROM start_time) AS hour,
 EXTRACT (DAY FROM start_time) AS day,
 EXTRACT (WEEKS FROM start_time) AS week,
@@ -200,7 +202,14 @@ FROM staging_events
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [staging_events_table_create,
+                        staging_songs_table_create, songplay_table_create,
+                        user_table_create, song_table_create,
+                        artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop,
+                      songplay_table_drop, user_table_drop, song_table_drop,
+                      artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [songplay_table_insert, user_table_insert,
+                        song_table_insert, artist_table_insert,
+                        time_table_insert]
